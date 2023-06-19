84c206613927bca423e34d82603268acde641975
Syafitra Alfa Ridzi <ia1alfaridzi@gmail.com>
2023-06-20 06:16:37 +0700

    add(document): migration, model, controller, index

---
+++ app/Models/Document.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+    use HasFactory;
+
+    protected $table        = 'documents';
+    protected $primaryKey   = 'document_id';
+    protected $fillable     = ['title', 'description', 'file_path', 'status', 'uploaded_by'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+}
---
+++ database/migrations/2023_06_19_225252_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id('document_id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('file_path');
+            $table->string('status');
+            $table->unsignedBigInteger('uploaded_by');
+            $table->foreign('uploaded_by')->references('user_id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+};
